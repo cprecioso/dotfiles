381ffe25222da0c68c46a985d8d3d29aa7885a41
Carlos Precioso <github@precioso.design>
2024-04-16 20:30:07 +0200

    Separate XDG overrides to their own file

---
+++ chezmoi/dot_config/exact_zsh/xdg-overrides.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+# shellcheck shell=bash
+
+compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION"
+
+# XDG Overrides
+export ADOTDIR="$XDG_DATA_HOME"/antigen
+export ANDROID_HOME="${XDG_DATA_HOME}"/android
+export ASDF_DATA_DIR="${XDG_DATA_HOME}"/asdf
+export AWS_CONFIG_FILE="${XDG_CONFIG_HOME}"/aws/config
+export AWS_SHARED_CREDENTIALS_FILE="${XDG_CONFIG_HOME}"/aws/credentials
+export BUNDLE_USER_CACHE="$XDG_CACHE_HOME"/bundle
+export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME"/bundle
+export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME"/bundle
+export CARGO_HOME="${XDG_DATA_HOME}"/cargo
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}"/docker
+export GEM_HOME="${XDG_DATA_HOME}"/gem
+export GEM_SPEC_CACHE="${XDG_CACHE_HOME}"/gem
+export GNUPGHOME="${XDG_DATA_HOME}"/gnupg
+export GOPATH="${XDG_DATA_HOME}"/go
+export HISTFILE="$XDG_STATE_HOME"/zsh/history
+export LESSHISTFILE="$XDG_STATE_HOME"/less/history
+export MINIKUBE_HOME="${XDG_DATA_HOME}"/minikube
+export MYSQL_HISTFILE="$XDG_DATA_HOME"/mysql_history
+export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
+export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/pythonrc
+export RUSTUP_HOME="${XDG_DATA_HOME}"/rustup
+export VAGRANT_HOME="$XDG_DATA_HOME"/vagrant
+
+# shellcheck disable=SC2139
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
