d61a53f95a67a1710ce0570af01a365bd5fe6890
Carlos Precioso <github@precioso.design>
2025-08-29 10:18:42 +0200

    Add OMZ config, disable autoupdate

---
+++ chezmoi/dot_config/exact_antidote/omz-config.zsh
@@ -0,0 +1,7 @@
+# shellcheck shell=bash
+
+# iTerm integration
+zstyle ':omz:plugins:iterm2' shell-integration yes
+
+# Disable ohmyzsh auto-update, since we have an auto-update script for antidote
+zstyle ':omz:update' mode disabled
