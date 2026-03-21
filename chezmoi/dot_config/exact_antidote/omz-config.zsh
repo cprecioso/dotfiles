0234a71287b0754f2f374901ec749267e0cd5b87
Carlos Precioso <github@precioso.design>
2026-03-21 11:03:05 +0100

    Replace iTerm for Ghostty

--- chezmoi/dot_config/exact_antidote/omz-config.zsh
+++ chezmoi/dot_config/exact_antidote/omz-config.zsh
@@ -1,5 +1,2 @@
-# iTerm integration
-zstyle ':omz:plugins:iterm2' shell-integration yes
-
 # Disable ohmyzsh auto-update, since we have an auto-update script for antidote
 zstyle ':omz:update' mode disabled
